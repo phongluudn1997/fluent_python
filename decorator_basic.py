ded853d4c5e38e864b0ba24f45d2b646aa92d961
Huynh Phong Luu <luu.huynh@fiisoft.com>
2020-03-11 11:38:04 +0700

    add tracking timer decorator.

--- decorator_basic.py
+++ decorator_basic.py
@@ -1,13 +1,28 @@
+import time
+
+
 def decorator_function(func):
     def wrapper(*args, **kwargs):
         print(f'wrapper run before {func.__name__} run')
-        return func(*args, **kwargs)
+        func(*args, **kwargs)
     return wrapper
 
 
-@decorator_function
+def time_traking(func):
+    '''Practice with time tracking'''
+    def wrapper(*args, **kwargs):
+        then = time.time()
+        result = func(*args, **kwargs)
+        now = time.time()
+        print(f'{func.__name__} took {now - then}s for executing.')
+        return result
+    return wrapper
+
+
+@time_traking
 def print_something(msg):
-    print(msg)
+    time.sleep(1)
+    return msg
 
 
-print_something('hello world')
+print(print_something('hello world'))
